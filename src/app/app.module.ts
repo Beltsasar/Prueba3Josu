73337f442e7b2c5d86a905a924fb8d4a51b7ba41
Beltsasar <134458936+Beltsasar@users.noreply.github.com>
2024-11-13 13:09:25 -0300

    arreglando errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -15,7 +14,6 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule, 
     IonicModule.forRoot(), 
-    AngularFireModule.initializeApp(environment.firebase), // Inicializa Firebase
     AngularFirestoreModule, // Importa Firestore
     AppRoutingModule,
     ReactiveFormsModule,
